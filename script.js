c142e16d295504ca90528e4039e781a06dc0f07d
Marko Ordenski <ordenskim@gmail.com>
2017-02-13 20:57:28 +0100

    Init files

---
+++ script.js
@@ -0,0 +1,23 @@
+$(function() {
+
+	$('nav li a').hover(function() {
+
+		$(this).css({'font-size':'170%'});
+
+	}, function() {
+
+		$(this).css({'font-size':''});
+	});
+
+
+	$('.navbar-brand').hover(function() {
+
+		$(this).css({'font-size':'220%'});
+
+	}, function() {
+
+		$(this).css({'font-size':''});
+	});
+
+
+});
